e47063a9ba2cc9404934388f0d3bc202e2804bc1
xiao.yuxin.2012@vjc.sg <xiao.yuxin.2012@vjc.sg>
2017-10-21 20:39:11 +0800

    debug meta generator

--- uploader.py
+++ uploader.py
@@ -1,32 +1,35 @@
 from hashlib import md5, sha1
 from time import time
+import sys
 
-CLIENT_NAME = "pytorrent"
-CLIENT_ID = "PY"
+
+CLIENT_NAME = "p2p_peer1"
+CLIENT_ID = "peer1"
 CLIENT_VERSION = "0001"
 TRACKER_ADDR = '127.0.0.1'
 
 
 def make_info_dict(file):
-	""" Returns the info dictionary for a torrent file. """
+    """ Returns the info dictionary for a torrent file. """
+    print('before open the file')
+    with open(file) as f:
+        contents = f.read()
 
-	with open(file) as f:
-		contents = f.read()
+    piece_length = 524288	# TODO: This should change dependent on file size
 
-	piece_length = 524288	# TODO: This should change dependent on file size
+    info = {}
 
-	info = {}
+    info["piece length"] = piece_length
+    info["length"] = len(contents.encode('utf-8'))
+    print('length: ' + str(info['length']))
+    info["name"] = file
+    info["md5sum"] = md5(contents).hexdigest()
 
-	info["piece length"] = piece_length
-	info["length"] = len(contents)
-	info["name"] = file
-	info["md5sum"] = md5(contents).hexdigest()
-
-	# Generate the pieces
-	#pieces = slice(contents, piece_length)
-	#pieces = [ sha1(p).digest() for p in pieces ]
+    # Generate the pieces
+    #pieces = slice(contents, piece_length)
+    #pieces = [ sha1(p).digest() for p in pieces ]
     #info["pieces"] = collapse(pieces)
-	return info
+    return info
 
 def make_torrent_file(file = None):
     """ Returns the bencoded contents of a torrent file. """
@@ -37,26 +40,20 @@ def make_torrent_file(file = None):
     torrent["announce"] = TRACKER_ADDR
     torrent["creation date"] = int(time())
     torrent["created by"] = CLIENT_NAME
+    print('before slicing the file')
     torrent["info"] = make_info_dict(file)
     metaFileName = file + '.torrent'
+    print('meta file name: ' + metaFileName)
     with open(metaFileName, "w") as torrent_file:
-        torrent_file.write(torrent)
+        torrent_file.write(str(torrent))
 
-
-def statement_string_from_stdin(echo=True):
-    try:
-        while True:
-            line = input('enter query')
-            if echo:
-                print('printing input' + line)
-            yield line
-    except EOFError:
-        return
-
-
-class Uploader():
-    def run(self):
-        """ Start  generating torrent file. """
-        for s in statement_string_from_stdin(echo=True):
-            make_torrent_file(s)
-            print('finish generating torrent file')
+def run():
+    print('starting: ')
+    """ Start  generating torrent file. """
+    while(True):
+        print('enter file name: ')
+        line = sys.stdin.readline()
+        print('getting file name: ' + line)
+        make_torrent_file(line.strip())
+        print('finish generating torrent file')
+run()
