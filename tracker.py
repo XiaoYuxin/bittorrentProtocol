24a6ffc140eade75d82d96fb33dd518e6dcf99a2
Su Sumei <sumei.su@u.nus.edu>
2017-10-22 10:44:51 +0800

    Add TCP server in tracker

--- tracker.py
+++ tracker.py
@@ -3,10 +3,24 @@
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from threading import Thread
-from logging import basicConfig, info
+from logging import basicConfig, info, INFO
 from socket import inet_aton
 from urllib import urlopen
+from urlparse import parse_qs
+import SocketServer
+import json
 
+
+def decode_request(path):
+	""" Return the decoded request string. """
+
+	# Strip off the start characters
+	if path[:1] == "?":
+		path = path[1:]
+	elif path[:2] == "/?":
+		path = path[2:]
+
+	return parse_qs(path)
 
 def add_peer(torrents, info_hash, peer_id, ip, port):
 	""" Add the peer to the peer list. """
@@ -20,7 +34,7 @@ def add_peer(torrents, info_hash, peer_id, ip, port):
 	else:
 		torrents[info_hash] = [(peer_id, ip, port)]
 
-def make_peer_list(peer_list):
+def peer_list(peer_list):
 	""" Return an expanded peer list suitable for the client, given
 	the peer list. """
 
@@ -35,94 +49,38 @@ def make_peer_list(peer_list):
 
 	return peers
 
-class RequestHandler(BaseHTTPRequestHandler):
-	def do_GET(s):
-		""" Take a request, do some some database work, return a peer
-		list response. """
+class TCPHandler(SocketServer.BaseRequestHandler):
 
-		# Get the necessary info out of the request
-		info_hash = package["info_hash"][0]
-		ip = s.client_address[0]
-		port = package["port"][0]
-		peer_id = package["peer_id"][0]
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        b = b''
+        self.data = self.request.recv(1024).strip()
+        b += self.data
+        print "{} wrote:".format(self.client_address[0])
+        print b
+        d = json.loads(b)
+        print d
+        print type(d)
+        info_hash = d['info_hash']
+        print info_hash
+        peer_id = d['peer_id']
+        ip = self.client_address[0]
+        port = d['port']
+        add_peer(torrents, info_hash, peer_id, ip, port)
+        peers = peer_list(torrents[info_hash])
+        data = json.dumps(peers)
+        # just send back the same data, but upper-cased
+        self.request.sendall(data.encode('utf8'))
 
-		add_peer(s.server.torrents, info_hash, peer_id, ip, port)
 
-		# Generate a response
-		response = {}
-		response["interval"] = s.server.interval
-		response["complete"] = 0
-		response["incomplete"] = 0
-		response["peers"] = make_peer_list( \
-		s.server.torrents[info_hash])
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
 
-		# Send off the response
-		s.send_response(200)
-		s.end_headers()
-		s.wfile.write(encode(response))
+    torrents = {}
 
-		# Log the request, and what we send back
-		info("PACKAGE: %s", package)
-		info("RESPONSE: %s", response)
+    # Create the server, binding to localhost on port 9999
+    server = SocketServer.TCPServer((HOST, PORT), TCPHandler)
 
-	def log_message(self, format, *args):
-		""" Just supress logging. """
-
-		return
-
-class Tracker():
-	def __init__(self, host = "", port = 9010, interval = 5, log = "tracker.log"):
-		""" Read in the initial values, load the database. """
-
-		self.host = host
-		self.port = port
-
-		self.server_class = HTTPServer
-		self.httpd = self.server_class((self.host, self.port), \
-			RequestHandler)
-
-		self.running = False	# We're not running to begin with
-
-		self.server_class.interval = interval
-
-		self.server_class.torrents = []
-
-		# Set logging info
-		basicConfig(filename = log, level = INFO)
-
-	def runner(self):
-		""" Keep handling requests, until told to stop. """
-
-		while self.running:
-			self.httpd.handle_request()
-
-	def run(self):
-		""" Start the runner, in a seperate thread. """
-
-		if not self.running:
-			self.running = True
-
-			self.thread = Thread(target = self.runner)
-			self.thread.start()
-
-	def send_dummy_request(self):
-		""" Send a dummy request to the server. """
-
-		# To finish off httpd.handle_request()
-		address = "http://127.0.0.1:" + str(self.port)
-		urlopen(address)
-
-	def stop(self):
-		""" Stop the thread, and join to it. """
-
-		if self.running:
-			self.running = False
-
-			self.send_dummy_request()
-			self.thread.join()
-
-	def __del__(self):
-		""" Stop the tracker thread, write the database. """
-
-		self.stop()
-		self.httpd.server_close()
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
