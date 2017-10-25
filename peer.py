c71209b4a8b8c99493477f327aa2cca46fcc0261
xiao.yuxin.2012@vjc.sg <xiao.yuxin.2012@vjc.sg>
2017-10-25 17:35:48 +0800

    add a new thread for run server

--- peer.py
+++ peer.py
@@ -129,7 +129,8 @@ class Torrent:
             [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [
                 [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in
                  [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
-        self.run_server()
+        run_server_loop = Thread(target=self.run_server())
+        run_server_loop.start()
         self.info_hash = None
         self.tracker_ip = None
         self.tracker_port = None
